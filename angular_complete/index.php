227f385d62518554607f1f4f0e7d1164cd55c4b4
Ken Ronoh <kennethronoh1@gmail.com>
2021-07-22 17:24:37 +0300

    Add files via upload

---
+++ angular_complete/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="app">
+<head>
+    <meta charset="utf-8">
+    <title>AngularJS CRUD with Search, Sort and Pagination</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+	  <div class="row">
+				<div class="col-md-12">
+					<span class="pull-right">
+						<input type="text" ng-model="search" class="form-control" placeholder="Search">
+					</span>
+				</div>
+			</div>
+    </div>
+  </div>
+</nav>
+
+
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      <span class="fs-4">Sidebar</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link active" aria-current="page">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+          Orders
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+          Products
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+          Customers
+        </a>
+      </li>
+    </ul>
+    <hr>
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+        <strong>mdo</strong>
+      </a>
+      <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2" style="">
+        <li><a class="dropdown-item" href="#">New project...</a></li>
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      </ul>
+    </div>
+  </div>
+
+</head>
+<body ng-controller="memberdata" ng-init="fetch()">
+<div class="container">
+    <h1 class="page-header text-center">AngularJS CRUD with Search, Sort and Pagination</h1>
+    <div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="alert alert-success text-center" ng-show="success">
+				<button type="button" class="close" ng-click="clearMessage()"><span aria-hidden="true">&times;</span></button>
+				<i class="fa fa-check"></i> {{ successMessage }}
+			</div>
+			<div class="alert alert-danger text-center" ng-show="error">
+				<button type="button" class="close" ng-lick="clearMessage()"><span aria-hidden="true">&times;</span></button>
+				<i class="fa fa-warning"></i> {{ errorMessage }}
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<button href="" class="btn btn-primary" ng-click="showAdd()"><i class="fa fa-plus"></i> New Member</button>
+					<span class="pull-right">
+						<input type="text" ng-model="search" class="form-control" placeholder="Search">
+					</span>
+				</div>
+			</div>
+			<table class="table table-bordered table-striped" style="margin-top:10px;">
+				<thead>
+                    <tr>
+                        <th ng-click="sort('firstname')" class="text-center">First Name
+                        	<span class="pull-right">
+                       			<i class="fa fa-sort gray" ng-show="sortKey!='firstname'"></i>
+                       			<i class="fa fa-sort" ng-show="sortKey=='firstname'" ng-class="{'fa fa-sort-asc':reverse,'fa fa-sort-desc':!reverse}"></i>
+                       		</span>
+                        </th>
+                        <th ng-click="sort('lastname')" class="text-center">Last Name
+	                        <span class="pull-right">
+	                       		<i class="fa fa-sort gray" ng-show="sortKey!='lastname'"></i>
+	                       		<i class="fa fa-sort" ng-show="sortKey=='lastname'" ng-class="{'fa fa-sort-asc':reverse,'fa fa-sort-desc':!reverse}"></i>
+	                       	</span>
+                        </th>
+                        <th ng-click="sort('address')" class="text-center">Address
+                        	<span class="pull-right">
+                       			<i class="fa fa-sort gray" ng-show="sortKey!='address'"></i>
+                       			<i class="fa fa-sort" ng-show="sortKey=='address'" ng-class="{'fa fa-sort-asc':reverse,'fa fa-sort-desc':!reverse}"></i>
+                       		</span>
+                       	</th>
+                       	<th class="text-center">Action</th>
+                    </tr>
+                </thead>
+				<tbody>
+					<tr dir-paginate="member in members|orderBy:sortKey:reverse|filter:search|itemsPerPage:5">
+						<td>{{ member.firstname }}</td>
+						<td>{{ member.lastname }}</td>
+						<td>{{ member.address }}</td>
+						<td>
+							<button type="button" class="btn btn-success" ng-click="showEdit(); selectMember(member);"><i class="fa fa-edit"></i> Edit</button> 
+							<button type="button" class="btn btn-danger" ng-click="showDelete(); selectMember(member);"> <i class="fa fa-trash"></i> Delete</button>
+						</td>
+
+					</tr>
+				</tbody>
+			</table>
+			<div class="pull-right" style="margin-top:-30px;">
+				<dir-pagination-controls
+				   max-size="5"
+				   direction-links="true"
+				   boundary-links="true" >
+				</dir-pagination-controls>
+			</div>
+		</div>
+	</div>
+	<?php include('modal.php'); ?>
+</div>
+<script src="dirPaginate.js"></script>
+<script src="angular.js"></script>
+</body>
+</html>
